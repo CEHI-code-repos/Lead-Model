8cf62a197a3246a91d409d865635e0b46766f28d
Man Chong Leong <ml69@rice.edu>
2018-04-26 10:21:27 -0500

    fix bug for the bash script

---
+++ cpAndGit.sh
@@ -0,0 +1,5 @@
+#mount_smbfs //ml69@crate.rice.edu/ProjectData/mlm/R72330 ~/Desktop/Crate/
+cp leadModel.R ~/Desktop/Crate/Projects/Unfunded/Lead/NC/Leadmodel_reworked/R_version/
+git add leadModel.R
+git commit -m "$1"
+git push
